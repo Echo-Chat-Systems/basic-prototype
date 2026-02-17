230cac27d6f01305726fa26bd0870754e5f98220
Yuki-42 <kylepannan@gmail.com>
2026-02-17 16:32:20 +1000

    Created SigninChallengeParameters

---
+++ EchoLib/Models/Params/Auth/SigninChallengeParameters.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EchoLib.Models.Params.Auth;
+
+public class SigninChallengeParameters : IParam
+{
+	public string Action => "signin-challenge";
+	
+	[JsonPropertyName("sign-challenge")] public string SignChallenge { get; }
+	[JsonPropertyName("encrypt-challenge")] public string EncryptChallenge { get; }
+}
