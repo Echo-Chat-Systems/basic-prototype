f276ca9e55569569a0c0d9d443313c0369f387eb
Yuki42_ <kylepannan@gmail.com>
2026-08-04 19:49:46 +1000

    Cleanup project - remove dead projects

--- TuiClient/Events/ServerNameChanged.cs
+++
@@ -1,8 +0,0 @@
-namespace TuiClient.Events;
-
-public class ServerNameChangedEventArgs(string? oldName, string? name)
-{
-	public string? OldName { get; } = oldName;
-	public string NewName { get; } = name;
-}
-
